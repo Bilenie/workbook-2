9f77c4e66110c8f2d9a3faee7aeedeb93922195d
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-16 19:43:53 -0500

    Build Full Name Parser

---
+++ FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameParser {
+
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        //prompts the user to enter a name in two kind of formating options.
+        System.out.println("Enter your first name => use this format (first last or first middle last):");
+        //store the answer in a string variable and trim the name using trim()
+        String fullName = myScanner.nextLine().trim();
+
+
+        //parse the name and print pieces information by using split()
+
+        String[] fullNameSplit = fullName.split(" ");
+
+        if (fullNameSplit.length == 2) {
+
+
+            //store the split pieces in a variable
+
+            String first = fullNameSplit[0];
+            String last = fullNameSplit[1];
+
+            //print first & last name if the condition is true.
+
+            System.out.println("First name: " + first);
+            System.out.println("Middle name: (none)");
+            System.out.println("Last name: " + last);
+        } else if (fullNameSplit.length == 3) {
+
+            //store the split pieces in a variable
+
+            String first = fullNameSplit[0];
+            String middle = fullNameSplit[1];
+            String last = fullNameSplit[2];
+
+            // Convert middle name to capital initial + "."
+            String middleInitial = middle.substring(0, 1).toUpperCase() + ".";
+
+            //print first & last name if the condition is true.
+
+            System.out.println("First name: " + first);
+            System.out.println("Middle name:" + middleInitial);
+            System.out.println("Last name: " + last);
+
+        } else {
+            System.out.println("please provide valid answer");
+        }
+
+    }
+}
