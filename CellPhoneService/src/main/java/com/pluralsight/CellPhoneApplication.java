dfb5332bd7027827c875808cefcc1e6704935ba3
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-18 14:53:15 -0500

    Build Cell Phone Application.

---
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+
+    static Scanner  myScanner = new Scanner (System.in);
+
+    public static void main(String[] args) {
+
+        CellPhone myPhone = new CellPhone();
+
+        System.out.println("What is the serial number");//we are going to pars it to int later.
+        String  sn = myScanner.nextLine();
+
+        System.out.println("What model is the phone");
+        String  model = myScanner.nextLine();
+
+        System.out.println("Who is the carrier");
+        String  carrier = myScanner.nextLine();
+
+        System.out.println("What is the phone number");
+        String  phoneNumber = myScanner.nextLine();
+
+        System.out.println("Who is the owner of the phone");
+        String  owner = myScanner.nextLine();
+
+        //set the cellphone data on the object
+
+        myPhone.setSerialNumber( Integer.parseInt(sn));
+        myPhone.setModel(model);
+        myPhone.setCarrier(carrier);
+        myPhone.setPhoneNumber(phoneNumber);
+        myPhone.setOwner(owner);
+
+        //use the getters to display this information
+
+        System.out.println("The SN is :" +  myPhone.getSerialNumber());
+        System.out.println("The Model is :" +  myPhone.getModel());
+        System.out.println("The Carrier is :" +  myPhone.getCarrier());
+        System.out.println("The Phone Number is :" +  myPhone.getPhoneNumber());
+        System.out.println("The Owner is :" +  myPhone.getOwner());
+
+    }
+
+}
