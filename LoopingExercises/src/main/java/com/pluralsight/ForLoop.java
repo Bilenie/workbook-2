fdcac71cc0d2a55535b139e427bdfb76f8879cd5
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-21 16:14:52 -0500

    exausting add

--- LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -1,15 +1,99 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class ForLoop {
     public static void main(String[] args) {
-
 
       for(int i =0;i<10;i++){
 
           System.out.println(i);
         }
         System.out.println("Time up!!!");
+//
+//        String s1 = "hello world";
+//
+//        System.out.println(s1.toUpperCase());
+//        int x = 5;
+//
+//        int y = ++x;
+//        System.out.println(y);
+//        String s1 = "Java";
+//
+//        String s2 = "Java";
+//
+//        String s3 = new String("Java");
+//
+//        System.out.println(s1 == s2);
+//
+//        System.out.println(s1 == s3);
+//
+//        System.out.println(s1.equals(s3));
 
+//        String s1 = "hello";
+//
+//        String s2 = "HELLO";
+//
+//        System.out.println(s1.equals(s2));
 
+//        int x = 9;
+//
+//        if (x++ < 10) {
+//
+//            System.out.println("Low");
+//
+//        } else {
+//
+//            System.out.println("High");
+//
+//        }
+
+//        int x = 5;
+//
+//        System.out.println(x++);
+//        int x = 10;
+//
+//        if (x < 5 ) {
+//
+//            System.out.println("Low");
+//
+//        } else if (x < 15) {
+//
+//            System.out.println("Medium");
+//
+//        } else {
+//
+//            System.out.println("High");
+//
+//        }
+//        int i = 10;
+//
+//        while (i > 0) {
+//
+//            System.out.print(i + " ");
+//
+//            i -= 2;
+//
+//        }
+        //This I dont understand
+//        for (int i = 0; i < 5; i++) {
+//
+//            if (i == 2) {
+//
+//                continue;
+//
+//            }
+//
+//            System.out.print(i + " ");
+//
+//        }
+//        int x = 5;
+//
+//        x += 3;
+//        System.out.println(x);
+//        int x = 5;
+//
+//        int y = ++x;
+//        System.out.println(x);
     }
 }
